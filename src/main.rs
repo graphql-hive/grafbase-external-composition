96dca15a902a356b69bd0b1184b5095522bfda2c
Dotan Simha <dotansimha@gmail.com>
2026-04-04 10:51:07 +0300

    good

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
 use ntex::web;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+pub struct SchemaService {
+    pub sdl: String,
+    pub name: String,
+    pub url: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+pub enum CompositionResult {
+    Success { result: CompositionSuccessResult },
+    Failure { result: CompositionFailureResult },
+}
+
+#[derive(Debug, Serialize)]
+pub struct CompositionSuccessResult {
+    pub supergraph: String,
+    pub sdl: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CompositionFailureResult {
+    pub errors: Vec<CompositionError>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CompositionError {
+    pub message: String,
+    pub source: ErrorSource,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ErrorSource {
+    GraphQL,
+    Composition,
+}
 
 async fn hello() -> &'static str {
     "hello-world"
@@ -6,10 +45,8 @@ async fn hello() -> &'static str {
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
-    web::HttpServer::new(|| {
-        web::App::new().route("/", web::get().to(hello))
-    })
-    .bind("0.0.0.0:4000")?
-    .run()
-    .await
+    web::HttpServer::new(|| web::App::new().route("/", web::get().to(hello)))
+        .bind("0.0.0.0:4000")?
+        .run()
+        .await
 }
